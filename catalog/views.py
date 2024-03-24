bc1738bee9d05d9585db09f96959093bd00291ab
Ivan Skliaruk <cunanori@gmail.com>
2024-03-24 03:59:46 +0100

    hw1

---
+++ catalog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Пользователь {name} ({phone}) оставил сообщение: {message}')
+
+    return render(request, 'catalog/contacts.html')
